afe1b7e59cbc3ddeb954dcbbe5d16d5fbdfa43db
Macarthur Inbody <133794m3r@gmail.com>
2019-07-14 23:29:57 -0400

    Included my c programs and some other things. Also made sure that my scripts and current state of things is working right now. It'll require more work tomorrow.

--- c_programs/givemethepin.c
+++ c_programs/givemethepin.c
@@ -1,49 +1,83 @@
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stropts.h>
-#include <string.h>
 #include <unistd.h>
 /**
-* The basic C Program that they're going to be brute forcing.
-*
+* The basic C Program that they're going to be brute forcing the pin from.
+* Macathur Inbody <mdi2455@email.vccs.edu>
+* 2019-
+* AGPLv3 or Later
+*/
+
+/**
+* Basic base64 encoder.
+* t=tuplet aka 3 bytes of data.
+* a=first byte.
+* b=second byte.
+* c=third byte.
+* I'm using shifting so that it's a bit faster. also the table is the same as the
+* base64 program from bash so that they can actually give it the same thing.
+*/
+int base64_encode(char *dest, char *src, int srclen){
+    const char table[64]="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+    unsigned int i=0;
+    unsigned int j=0;
+    unsigned int a=0;
+    unsigned int b=0;
+    unsigned int c=0;
+    unsigned int t=0;
+    const char padding[1]="=";
+    while (i < srclen){
+        a=i < srclen ? src[i++] : 0;
+        b=i < srclen ? src[i++] : 0;
+        c=i < srclen ? src[i++] : 0;
+        t= (a << 16) + (b << 8) + c;
+        dest[j++]=table[ (t >> (3*6)) & 63];
+        dest[j++]=table[ (t >> (2*6)) & 63];
+        dest[j++]=table[ (t >> (1*6)) & 63];
+        dest[j++]=table[ (t>>0) & 63];
+    }
+    if( ( srclen % 3) > 0){
+        dest[j-1]=padding[0];
+        if(( srclen %3) == 1){
+            dest[j-2]=padding[0];
+        }
+    }
+    return 0;
+}
+/**
+* The flag is going to be replaced with the value <pinflag> so that it's more
+* easily edited with sed in a future version. Also the correct_pin string will
+* similarly be changed too once I make sure that it works to be <correct_pin>
+* so that I can sed it away more easily.
+* 
 *
 */
-int base64_encode(unsigned char *dest, const unsigned char *src, int srclen){
-const char table[64]="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-unsigned int i=0;
-unsigned int j=0;
-unsigned int a=0;
-unsigned int b=0;
-unsigned int c=0;
-unsigned int t=0;
-while (i < srclen){
-    a=i < srclen ? src[i++] : 0;
-    b=i < srclen ? src[i++] : 0;
-    c=i < srclen ? src[i++] : 0;
-    t= (a << 16) + (b << 8) + c;
-    dest[j++]=table[ (t >> (3*6)) & 63];
-    dest[j++]=table[ (t >> (2*6)) & 63];
-    dest[j++]=table[ (t >> (1*6)) & 63];
-    dest[j++]=table[ (t>>0) & 63];
-}
-return 0;
-}
 int main(int argc, char **argv){
-char correct_pin_string[]="MTUw";
-char *flag_string="The flag is ";
-char flag[]="-vGvr4t.3xrfx-M56kZg5f-HyUw";
-if(argc > 1){
-printf("This program doesn't take arguments.\n");
-return -1;
-}
-size_t len;
-int bytes=0;
-int read=0;
-int str_compared=0;
-int return_value=0;
+    char *correct_pin=malloc(3);
+    //correct_pin="111";
+    correct_pin="<correct_pin_placeholder>";
+    //char correct_pin_string[4]="MTUw";
+    char *correct_pin_string=malloc(4);
+    char *flag_string="The flag is ";
+    char flag="<the_flag_placeholder>";
+    char incorrect_flag1="<incorrect_flag_placeholder1>";
+    char incorrect_flag2="<incorrect_flag_placeholder2>";
+    //char flag[28]="-vGvr4t.3xrfx-M56kZg5f-HyUw";
+    //char incorrect_flag1[28]="-vGvr4t.3xrfx-M56kZg5f-HyUy";
+    //char incorrect_flag2[28]="-vGvr4t.3xrfx-M56kZg5f-HyUz";        
+    if(argc > 1){
+        printf("This program doesn't take arguments.\n");
+        return -1;
+    }
+    base64_encode(correct_pin_string,correct_pin,3);
+    size_t len;
+    int bytes=0;
+    int read=0;
+    int str_compared=0;
+    int return_value=0;
     fd_set read_file_descriptors;
     FD_ZERO(&read_file_descriptors);
-    
     struct timeval never_wait;
     never_wait.tv_sec = 0;
     never_wait.tv_usec = 0;
@@ -51,22 +85,35 @@ int return_value=0;
     sprintf(flag_msg,"%s%s",flag_string,flag);
     char message[8];
     FD_SET(STDIN_FILENO,&read_file_descriptors);
-if(select(1,&read_file_descriptors,NULL,NULL,&never_wait)){
-    scanf("%s",message);
-    str_compared=strcmp(correct_pin_string,message);
-    if(str_compared == 0){
-    fprintf(stderr,flag_msg);
-    fprintf(stdout,"The flmJ3nmx7QbBUtB3kNw6RS1Yb56RE6D3iW85nt6oUZeyFFrtDwsg3SEw\n");
+    if(select(1,&read_file_descriptors,NULL,NULL,&never_wait)){
+        scanf("%s",message);
+        str_compared=strcmp(correct_pin_string,message);
+        if(strlen(message) == 8){
+            fprintf(stderr,"You should run it as `echo -n | base64` to make sure that newline isn't included.\n");
+            fprintf(stdout,"You should run it as `echo -n | base64` to make sure that newline isn't included.");
+        }
+        else{
+            if(str_compared == 0){
+            /**
+            * Flag1 will be replaced by string <false_flag1>
+            * Flag 2 will be replaced by string <false_flag2>
+            * TODO:setup 2 false flags that are changed via the strings.
+            * The false flags will just be data read from /dev/urandom that's base64 encoded.
+            */
+            fprintf(stdout,"The flag is %s\n",incorrect_flag1);
+            fprintf(stderr,"%s\n",flag_msg);
+            fprintf(stdout,"The flag is %s\n",incorrect_flag2);
+            fprintf(stdout,"Which flag is the correct one?\n");
+            }
+            else{
+            fprintf(stderr,"The pin you've entered is incorrect.\nPlease try again.\n");
+            }
+            return_value=0;
+       }
     }
     else{
-    fprintf(stderr,"The pin you've entered is incorrect.\nPlease try again.\n");
+        fprintf(stderr,"Nothing given.\n");
+        return_value=-1;
     }
-    return_value=0;
-    }
-else{
-//printf("ioctl :%d \n",bytes);
-fprintf(stderr,"Nothing given.\n");
-return_value=-1;
-}
-return return_value;
+    return return_value;
 }
