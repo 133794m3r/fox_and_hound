d6a390e3a4299a0c2bc71155a2dc3992d09ff2ce
Macarthur Inbody <133794m3r@gmail.com>
2019-07-18 11:01:53 -0400

    Added a base32 encoder for use in my building of flags so that students can type them in more easily.

---
+++ c_programs/b32_encoder.c
@@ -0,0 +1,118 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdint.h>
+/**
+* The basic C Program that they're going to be brute forcing the pin from.
+* Macathur Inbody <mdi2455@email.vccs.edu>
+* 2019-
+* AGPLv3 or Later
+*/
+static inline unsigned char uchar(char chr){ return chr;};
+/**
+* This function encodes a string passed to it as a bas32 string.
+*
+*/
+int base32_encode(char *dest, const char *src, unsigned int srclen, unsigned int outlen){
+    const char table[32]="ybndrfg8ejkmcpqxot1uwisza345h769";
+    //const char table[32]="ABCDEFGHIJKLMNOPQRSTUVWXYZ234567";
+    unsigned int i=0;
+    unsigned int j=0;
+    unsigned char a=0;
+    unsigned char b=0;
+    unsigned char c=0;
+    unsigned char d=0;
+    unsigned char e=0;
+  //  unsigned int t=0;
+    unsigned char leftover_bytes=(srclen % 5);
+    unsigned char replace_bytes=0;
+    const char padding[1]="=";
+    unsigned int iters=(srclen / 5);
+    while (i < srclen && j< outlen){
+           
+        a=i < srclen ? uchar(src[i++]) : 0;
+        b=i < srclen ? uchar(src[i++]) : 0;
+        c=i < srclen ? uchar(src[i++]) : 0;
+        d=i < srclen ? uchar(src[i++]) : 0;
+        e=i < srclen ? uchar(src[i++]) : 0;
+ //       t= (a << 16) + (b << 8) + c;
+/*
+(a >> 3)
+( ((a & 0x07) << 2) | (b >> 6))
+( ((b & 0x3F) >> 1) )
+( ((b & 0x01) << 4) | (c >> 4))
+( ((c & 0x0F) << 1) | (d >> 7))
+( ((d & 0x7F) >> 2))
+( ((d & 0x03) << 3) | (e >> 5))
+( ((e & 0x1F) ))
+*/  
+    dest[j++]=table[(a >> 3)];
+    dest[j++]=table[( ((a & 0x07) << 2) | (b >> 6))];
+    dest[j++]=table[( ((b & 0x3F) >> 1) )];
+    dest[j++]=table[( ((b & 0x01) << 4) | (c >> 4))];
+    dest[j++]=table[( ((c & 0x0F) << 1) | (d >> 7))];
+    dest[j++]=table[( ((d & 0x7F) >> 2))];
+    dest[j++]=table[( ((d & 0x03) << 3) | (e >> 5))];
+    dest[j++]=table[( ((e & 0x1F) ))];      
+    }
+    
+    if (leftover_bytes == 1) replace_bytes = 6;
+    else if (leftover_bytes == 2) replace_bytes = 4;
+    else if (leftover_bytes == 3) replace_bytes = 3;
+    else if (leftover_bytes == 4) replace_bytes = 1;
+    j--;
+    for(i=0;i<replace_bytes;i++){
+        dest[j--]=padding[0];
+    }
+    return 0;
+}
+int main(int argc,char **argv){
+    char *dest=malloc(sizeof *dest);
+    char *help="-h";
+    char *src_buffer=malloc(sizeof *src_buffer);
+    char *total_buffer=malloc(sizeof *total_buffer);
+    char *src;
+    struct timeval never_wait;   
+    never_wait.tv_sec = 0;
+    never_wait.tv_usec = 1;
+    unsigned int srclen=0;     
+    unsigned int outlen=0;
+    fd_set read_file_descriptors;
+    FD_ZERO(&read_file_descriptors);
+    FD_SET(STDIN_FILENO,&read_file_descriptors);     
+    if(argc > 1 && strncmp(argv[1],help,3) != 0){
+        src=argv[1];
+        srclen=strlen(src);
+        outlen=(srclen*8/5);
+        outlen=outlen+(8-(outlen % 8 ));
+        dest=malloc(outlen);        
+        base32_encode(dest,src,srclen,outlen);
+        printf("%s",dest);
+    }
+    
+    else if(select(1,&read_file_descriptors,NULL,NULL,&never_wait)){
+        int read_bytes=0;
+        int read_status=0;
+        size_t len=0;
+        read_status = getline(&src_buffer, &len, stdin);    
+        while (read_status != -1){
+        if(read_status!=-1){
+            read_bytes+=read_status;
+            strcat(total_buffer,src_buffer);
+            src_buffer=NULL;            
+        }
+       
+            read_status = getline(&src_buffer, &len, stdin);
+        }
+        outlen=(read_bytes*8/5);
+        outlen=outlen+(8-(outlen % 8 ));
+        dest=malloc(outlen);
+        base32_encode(dest,total_buffer,read_bytes,outlen);
+        printf("%s",dest);
+    }
+    else{
+        printf("%s","If no arguments given it reads from stdin until it's at the end. Otherwise it reads the first argument passed to the program as a string to encode.\n");
+    }
+    return 0;
+}
---
+++ c_programs/givemethepin-edited.c
@@ -0,0 +1,123 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+/**
+* The basic C Program that they're going to be brute forcing the pin from.
+* Macathur Inbody <mdi2455@email.vccs.edu>
+* 2019-
+* AGPLv3 or Later
+*/
+
+/**
+* Basic base64 encoder.
+* t=tuplet aka 3 bytes of data.
+* a=first byte.
+* b=second byte.
+* c=third byte.
+* I'm using shifting so that it's a bit faster. also the table is the same as the
+* base64 program from bash so that they can actually give it the same thing.
+*/
+int base64_encode(unsigned char *dest, unsigned char *src, int srclen){
+    const char table[64]="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+    unsigned int i=0;
+    unsigned int j=0;
+    unsigned int a=0;
+    unsigned int b=0;
+    unsigned int c=0;
+    unsigned int t=0;
+    const char padding[1]="=";
+    while (i < srclen){
+        a=i < srclen ? src[i++] : 0;
+        b=i < srclen ? src[i++] : 0;
+        c=i < srclen ? src[i++] : 0;
+        t= (a << 16) + (b << 8) + c;
+        dest[j++]=table[ (t >> (3*6)) & 63];
+        dest[j++]=table[ (t >> (2*6)) & 63];
+        dest[j++]=table[ (t >> (1*6)) & 63];
+        dest[j++]=table[ (t>>0) & 63];
+    }
+    if( ( srclen % 3) > 0){
+        dest[j-1]=padding[0];
+        if(( srclen %3) == 1){
+            dest[j-2]=padding[0];
+        }
+    }
+    return 0;
+}
+/**
+* The flag is going to be replaced with the value <pinflag> so that it's more
+* easily edited with sed in a future version. Also the correct_pin string will
+* similarly be changed too once I make sure that it works to be <correct_pin>
+* so that I can sed it away more easily.
+* 
+*
+*/
+int main(int argc, char **argv){
+    char *correct_pin=malloc(3);
+    //correct_pin="111";
+    correct_pin="87";
+    //char correct_pin_string[4]="MTEx";
+    char *correct_pin_string=malloc(4);
+    char *flag_string="The flag is ";
+    char *flag=malloc(28);
+    char *incorrect_flag1=malloc(28);
+    char *incorrect_flag2=malloc(28);
+    
+    
+    flag="9uusTDCfzE2MbhpT3tc0R9rwG5o";
+    incorrect_flag1="Nl5fo2fp3U1AwyPMvwZIj_6FIdg";
+    incorrect_flag2="xAWAIn1TsR7EXZsUrX4yr-f_FDI";
+    /*
+    flag="-vGvr4t.3xrfx-M56kZg5f-HyUw";
+    incorrect_flag1="-vGvr4t.3xrfx-M56kZg5f-HyUy";
+    incorrect_flag2="-vGvr4t.3xrfx-M56kZg5f-HyUz";        
+    */
+    if(argc > 1){
+        printf("This program doesn't take arguments.\n");
+        return -1;
+    }
+    base64_encode((unsigned char*)correct_pin_string,(unsigned char*)correct_pin,strlen(correct_pin));
+    int str_compared=0;
+    int return_value=0;
+    fd_set read_file_descriptors;
+    FD_ZERO(&read_file_descriptors);
+    struct timeval never_wait;
+    never_wait.tv_sec = 0;
+    never_wait.tv_usec = 0;
+    char flag_msg[41];
+    sprintf(flag_msg,"%s%s",flag_string,flag);
+    char message[8];
+    FD_SET(STDIN_FILENO,&read_file_descriptors);
+    if(select(1,&read_file_descriptors,NULL,NULL,&never_wait)){
+        scanf("%s",message);
+        str_compared=strncmp(correct_pin_string,message,4);
+        if(strlen(message) == 8){
+            fprintf(stderr,"You should run it as `echo -n | base64` to make sure that newline isn't included.\n");
+            fprintf(stdout,"You should run it as `echo -n | base64` to make sure that newline isn't included.");
+        }
+        else{
+            if(str_compared == 0){
+            /**
+            * Flag1 will be replaced by string <false_flag1>
+            * Flag 2 will be replaced by string <false_flag2>
+            * TODO:setup 2 false flags that are changed via the strings.
+            * The false flags will just be data read from /dev/urandom that's base64 encoded.
+            */
+            fprintf(stdout,"The flag is %s\n",incorrect_flag1);
+            fprintf(stderr,"%s\n",flag_msg);
+            fprintf(stdout,"The flag is %s\n",incorrect_flag2);
+            fprintf(stdout,"Which flag is the correct one?\n");
+            }
+            else{
+            fprintf(stderr,"The pin you've entered is incorrect.\nPlease try again.\n");
+            }
+            return_value=0;
+       }
+    }
+    else{
+        fprintf(stderr,"Nothing given.\n");
+        return_value=-1;
+    }
+    return return_value;
+}
