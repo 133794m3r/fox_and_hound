9e590f033fe112da0e5624fac242fecae6d97e89
Macarthur Inbody <133794m3r@gmail.com>
2019-07-18 11:01:53 -0400

    Added a base32 encoder for use in my building of flags so that students can type them in more easily.

--- c_programs/test_b64.c
+++ c_programs/test_b64.c
@@ -2,35 +2,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <stdint.h>
 /**
 * The basic C Program that they're going to be brute forcing the pin from.
 * Macathur Inbody <mdi2455@email.vccs.edu>
 * 2019-
 * AGPLv3 or Later
 */
+static inline unsigned char uchar(char chr){ return chr;};
 /**
 * This function encodes a string passed to it as an argument into base64. It's
 * ~1.5x as fast as the built in base64 program from gnulib. I am not allowing for
 * pipes at this time though that is a feature for a future release.
 *
 */
-static inline unsigned char uchar(char chr){ return chr;};
-int base64_encode(unsigned char *dest, const unsigned char *src, int srclen){
+int base64_encode(char *dest, const char *src, int srclen){
     const char table[64]="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
     unsigned int i=0;
     unsigned int j=0;
-    unsigned int a=0;
-    unsigned int b=0;
-    unsigned int c=0;
+    unsigned char a=0;
+    unsigned char b=0;
+    unsigned char c=0;
     unsigned int t=0;
     const char padding[1]="=";
     while (i < srclen){
+           
         a=i < srclen ? uchar(src[i++]) : 0;
         b=i < srclen ? uchar(src[i++]) : 0;
         c=i < srclen ? uchar(src[i++]) : 0;
         t= (a << 16) + (b << 8) + c;
+
         dest[j++]=table[ (t >> (3*6)) & 63];
-        dest[j++]=table[ (t >> (2*6)) & 63];
+        dest[j++]=table[ (t >> (2*6)) & 63];  
         dest[j++]=table[ (t >> (1*6)) & 63];
         dest[j++]=table[ (t>>0) & 63];
     }
@@ -43,15 +46,15 @@ int base64_encode(unsigned char *dest, const unsigned char *src, int srclen){
     return 0;
 }
 int main(int argc,char **argv){
-    unsigned char *dest=malloc(14);
+    char *dest=malloc(sizeof *dest);
     char *help="-h";
     char *src_buffer=malloc(sizeof *src_buffer);
+    char *total_buffer=malloc(sizeof *total_buffer);
     char *src;
     struct timeval never_wait;   
     never_wait.tv_sec = 0;
-    never_wait.tv_usec = 0;
+    never_wait.tv_usec = 1;
     unsigned int srclen=0;     
-    char message[8];
     fd_set read_file_descriptors;
     FD_ZERO(&read_file_descriptors);
     FD_SET(STDIN_FILENO,&read_file_descriptors);     
@@ -59,20 +62,25 @@ int main(int argc,char **argv){
         src=argv[1];
         srclen=strlen(src);
         dest=malloc(srclen*4/3);
-        base64_encode((unsigned char*)dest,(unsigned char*)src,srclen);
+        base64_encode(dest,src,srclen);
         printf("%s",dest);
     }
+    
     else if(select(1,&read_file_descriptors,NULL,NULL,&never_wait)){
         int read_bytes=0;
         int read_status=0;
         size_t len=0;
         read_status = getline(&src_buffer, &len, stdin);    
         while (read_status != -1){
-        read_status = getline(&src_buffer, &len, stdin);        
+        if(read_status!=-1){
+            read_bytes+=read_status;
+            strcat(total_buffer,src_buffer);
+            src_buffer=NULL;            
         }
-        src=src_buffer;
-        srclen=strlen(src);
-        base64_encode((unsigned char*)dest,(unsigned char*)src,srclen);
+       
+            read_status = getline(&src_buffer, &len, stdin);
+        }
+        base64_encode(dest,total_buffer,read_bytes);
         printf("%s",dest);
     }
     else{
