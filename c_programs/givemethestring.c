20ff2569d34c335642f1b6a031d5a05ba499ab8b
Macarthur Inbody <133794m3r@gmail.com>
2019-07-27 16:40:49 -0400

    Forgot to add the string bruteforcer function.

---
+++ c_programs/givemethestring.c
@@ -0,0 +1,43 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+/**
+* The basic C Program that they're going to be passing the string to get the flag
+* for the vim level.
+* Macathur Inbody <mdi2455@email.vccs.edu>
+* 2019-
+* AGPLv3 or Later
+*/
+
+int main(int argc, char **argv){
+    char *correct_string=malloc(8);
+    correct_string="<correct_string_placeholder>"
+    correct_string="12345678";
+    char *flag_string="The flag is ";    
+    char *flag=malloc(28);
+    //flag="ABCDEFGHIJKLMNO";
+    flag="<flag_placeholder_string>";
+    
+    /*
+    * This whole if else if thing is a hacky thing to avoid declaring another variable and
+    * having nested loops.
+    */
+    
+    if( argc > 2 ){
+        printf("The program only takes one argument.\n");
+    }
+    else if(argc == 1 ){
+        printf("No arguments given.\nStandard operation is %s <STRING>\n",argv[0]);
+    }
+    else if(argc == 0){
+        printf("This program doesn't read from stdin.\nStandard operation is %s <STRING>\n",argv[0]);
+    }
+    else if(! strncmp(correct_string,argv[1],8) ){
+        printf("\n%s %s\n",flag_string,flag);
+    }
+    else{
+        printf("Sorry you have guessed wrong.\n");
+    }
+    return 0;
+}
